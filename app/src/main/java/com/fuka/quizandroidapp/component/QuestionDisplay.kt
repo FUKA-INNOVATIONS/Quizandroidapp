6bffe36b758d033aefbd99a95487f657772f5b23
Fuwad Kalhori <fuad.kalhori@gmail.com>
2022-12-21 16:31:55 +0200

    question answer logic added

--- app/src/main/java/com/fuka/quizandroidapp/component/QuestionDisplay.kt
+++ app/src/main/java/com/fuka/quizandroidapp/component/QuestionDisplay.kt
@@ -1,19 +1,59 @@
 package com.fuka.quizandroidapp.component
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.PathEffect
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.fuka.quizandroidapp.model.QuestionItem
 import com.fuka.quizandroidapp.ui.AppColors
+import com.fuka.quizandroidapp.viewModel.QuestionsViewModel
 
-@Preview
+
 @Composable
-fun QuestionDisplay() {
-    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f,10f), phase = 0f)
+fun QuestionDisplay(
+    question: QuestionItem,
+    questionIndex: MutableState<Int>,
+    viewModel: QuestionsViewModel,
+    onNextClicked: (Int) -> Unit
+) {
+    val choicesState = remember(question) {
+        question.choices.toMutableList()
+    }
+    val answerState = remember(question) {
+        mutableStateOf<Int?>(null)
+
+    }
+    val correctAnswerState = remember(question) {
+        mutableStateOf<Boolean?>(null)
+
+    }
+
+    val updateAnswer: (Int) -> Unit = remember(question) {
+        {
+            answerState.value = it
+            correctAnswerState.value = choicesState[it] == question.answer
+        }
+
+    }
+
+    val pathEffect = PathEffect.dashPathEffect(floatArrayOf(10f, 10f), phase = 0f)
 
     Surface(
         modifier = Modifier
@@ -22,10 +62,112 @@ fun QuestionDisplay() {
             .padding(4.dp),
         color = AppColors.mDarkPurple
     ) {
-        Column(modifier = Modifier.padding(12.dp),
-            verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.Start) {
-            QuestionTracker()
+        Column(
+            modifier = Modifier.padding(12.dp),
+            verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.Start
+        ) {
+            QuestionTracker(counter = questionIndex.value)
             DrawDottedLine(pathEffect)
+
+            Column {
+                Text(
+                    text = question.question,
+                    modifier = Modifier
+                        .padding(6.dp)
+                        .align(alignment = Alignment.Start)
+                        .fillMaxHeight(0.3f),
+                    fontSize = 17.sp,
+                    color = AppColors.mOffWhite,
+                    fontWeight = FontWeight.Bold,
+                    lineHeight = 22.sp
+                )
+                //choices
+                choicesState.forEachIndexed { index, answerText ->
+                    Row(
+                        modifier = Modifier
+                            .padding(3.dp)
+                            .fillMaxWidth()
+                            .height(55.dp)
+                            .border(
+                                width = 4.dp,
+                                brush = Brush.linearGradient(
+                                    colors = listOf(
+                                        AppColors.mOffDarkPurple,
+                                        AppColors.mOffDarkPurple
+                                    )
+                                ),
+                                shape = RoundedCornerShape(15.dp)
+                            )
+                            .clip(
+                                RoundedCornerShape(
+                                    topStartPercent = 50,
+                                    topEndPercent = 50,
+                                    bottomEndPercent = 50,
+                                    bottomStartPercent = 50
+                                )
+                            )
+                            .background(Color.Transparent),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        RadioButton(
+                            selected = (answerState.value == index),
+                            onClick = {
+                                updateAnswer(index)
+                            },
+                            modifier = Modifier.padding(start = 16.dp),
+                            colors = RadioButtonDefaults
+                                .colors(
+                                    selectedColor = if (correctAnswerState.value == true && index == answerState.value) {
+                                        Color.Green.copy(alpha = 0.2f)
+                                    } else {
+                                        Color.Red.copy(alpha = 0.2f)
+                                    }
+                                )
+                        ) //end rb
+
+                        val annotatedString = buildAnnotatedString {
+                            withStyle(
+                                style = SpanStyle(
+                                    fontWeight = FontWeight.Light,
+                                    color = if (correctAnswerState.value == true && index == answerState.value) {
+                                        Color.Green
+                                    } else if (correctAnswerState.value == false && index == answerState.value) {
+                                        Color.Red
+                                    } else {
+                                        AppColors.mOffWhite
+                                    },
+                                    fontSize = 17.sp
+                                )
+                            ) {
+
+                                append(answerText)
+                            }
+                        }
+                        Text(text = annotatedString, modifier = Modifier.padding(6.dp))
+                    }
+                }
+                Button(onClick = { onNextClicked(questionIndex.value) },
+                    modifier = Modifier
+                        .padding(3.dp)
+                        .align(alignment = Alignment.CenterHorizontally),
+                    shape = RoundedCornerShape(34.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        backgroundColor = AppColors.mLightBlue
+                    ), content = {
+                        Text(
+                            text = "Next",
+                            modifier = Modifier.padding(4.dp),
+                            color = AppColors.mOffWhite,
+                            fontSize = 17.sp
+                        )
+
+                    })
+
+            }
+
         }
+
+
     }
+
 }
--- app/src/main/java/com/fuka/quizandroidapp/component/Questions.kt
+++ app/src/main/java/com/fuka/quizandroidapp/component/Questions.kt
@@ -3,6 +3,8 @@ package com.fuka.quizandroidapp.component
 import android.util.Log
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import com.fuka.quizandroidapp.util.Constants
 import com.fuka.quizandroidapp.viewModel.QuestionsViewModel
 
@@ -10,13 +12,23 @@ import com.fuka.quizandroidapp.viewModel.QuestionsViewModel
 fun Questions(viewModel: QuestionsViewModel) {
 
     val questions = viewModel.data.value.data?.toMutableList()
+
+    val questionIndex = remember {
+        mutableStateOf(0)
+    }
+
     if (viewModel.data.value.loading == true) {
         CircularProgressIndicator()
-        Log.d(Constants.LOG_TAG, "Questions: Loading..")
     } else {
-        Log.d(Constants.LOG_TAG, "Questions: Loading Stopped")
-        questions?.forEach { questionItem ->
-            // Log.d(Constants.LOG_TAG, "Question: ${questionItem.question}")
+
+        val question = try {
+            questions?.get(questionIndex.value)
+        } catch (ex: Exception) { null }
+
+        if (questions != null) {
+            QuestionDisplay(question = question!!, questionIndex = questionIndex, viewModel = viewModel) {
+                questionIndex.value = questionIndex.value + 1
+            }
         }
     }
 
